22f930d5ca76ae59d8fb623a9d75d0371d76e674
Blaine13Git <windsingcc@163.com>
2019-04-09 00:26:20 +0800

    GitKraken tool

---
+++ src/main/java/com/study/lion/lesson/test/Main.java
@@ -0,0 +1,7 @@
+package com.study.lion.lesson.test;
+
+public class Main extends com.study.lion.lesson.aboutArray.Main {
+    public static void main(String[] args) {
+        com.study.lion.lesson.aboutArray.Main case01 = new com.study.lion.lesson.aboutArray.Main();
+    }
+}
